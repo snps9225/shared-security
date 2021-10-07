ba544b990c0bfd95ea38eb0f81834a4ad12ea1d5
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-10-07 13:22:05 -0400

    Create inside_image.groovy

---
+++ vars/inside_image.groovy
@@ -0,0 +1,37 @@
+#!/usr/bin/env groovy
+
+package vars
+
+/*
+  A helper function for running pipeline code inside
+  pipeline image. 
+  
+  ex: 
+  inside_image "openshift_helm", {
+    sh "helm version"
+  }
+*/
+def call(String registry, String repository, String image, String creds, Closure body){
+ 
+  /*
+  config.images ?: { error "SDP Image Config not defined in Pipeline Config" } ()
+  
+  def sdp_img_reg = config.images.registry ?:
+                    { error "SDP Image Registry not defined in Pipeline Config" } ()
+  
+  def sdp_img_repo = config.images.repository ?:
+                     { return "sdp" }()
+                     
+  def sdp_img_repo_cred = config.images.cred ?:
+                          { error "SDP Image Repository Credential not defined in Pipeline Config" }()
+                          
+  def docker_args = config.images.docker_args ?:
+                    { return ""}()
+  */
+  
+  docker.withRegistry(registry, creds){
+    docker.image("${repository }/${image}").inside(""){
+      body()
+    }
+  }
+}
