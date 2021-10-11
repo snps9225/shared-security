5a3882ccc6b640bdd1da6cffe4372aa4a894358b
snps9225 <56947899+snps9225@users.noreply.github.com>
2021-10-11 16:42:18 -0400

    Update sast2.groovy

--- vars/sast2.groovy
+++ vars/sast2.groovy
@@ -6,27 +6,24 @@ package vars
 def call(Map config=[:]) {
     stage("Static Analysis: Checkmarx") {
         String project = config.Project
-	      project = project.replace('\\','\\\\')
+     	project = project.replace('\\','\\\\')
         String SASTHigh = config.High ?: '5'
-	      String SASTMedium = config.Medium ?: '5'
+      	String SASTMedium = config.Medium ?: '5'
         String CxServer = "https://checkmarx.corp.n-able.com" //"https://192.69.16.204"
         String userPreset = config.Preset ?: 'High and Medium'
         String creds = config.CxCred
-	      String lang = config.Language
+      	String lang = config.Language
         String preset = lang + " - " + userPreset //" - High and Medium" 
-	      String script = ""
+      	String script = ""
 
-	      println "OOO Preset is: " + preset
         inside_image "https://registry.hub.docker.com","karnc","checkmarx:openjdk-8","docker-creds", {
          	 	
-        dir("${WORKSPACE}")
-        {
-                  withChecks('SAST Scan') 
-          {
-            withCredentials([
-                          usernamePassword(credentialsId: creds, passwordVariable: 'CHECKMARX_PASS', usernameVariable: 'CHECKMARX_UNAME')
-                          ]) {
-                              script = """/opt/CxConsolePlugin/runCxConsole.sh scan -v \
+        dir("${WORKSPACE}") {
+		withChecks('SAST Scan') {
+			withCredentials([
+				usernamePassword(credentialsId: creds, passwordVariable: 'CHECKMARX_PASS', usernameVariable: 'CHECKMARX_UNAME')
+                          	]) {
+				script = """/opt/CxConsolePlugin/runCxConsole.sh scan -v \
                                       -ProjectName \"$project\" \\
                                       -CxServer \"CxServer\" \\
                                       -LocationType \"folder\" \\
@@ -40,15 +37,15 @@ def call(Map config=[:]) {
                                       -CxUser \"\$CHECKMARX_UNAME\" \\
                                       -CxPassword \"\$CHECKMARX_PASS\""""
 
-                              //def statusCode = sh (script:script, returnStatus:true)
+				      /*def statusCode = sh (script:script, returnStatus:true)
 
-                              if(statusCode == 0 || statusCode > 5) {
-                                  sh "cp /opt/CxConsolePlugin/Checkmarx/Reports/cx_output.xml ${WORKSPACE}"
-                                  archiveArtifacts artifacts: "cx_output.xml"
-                              }
-        }
-                  }
-	}
+				      if(statusCode == 0 || statusCode > 5) {
+					  sh "cp /opt/CxConsolePlugin/Checkmarx/Reports/cx_output.xml ${WORKSPACE}"
+					  archiveArtifacts artifacts: "cx_output.xml"
+				      }*/
+        			}
+                 	}
+		}
 	}
     }
 }
